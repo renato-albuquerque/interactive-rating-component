93e4c265b3cf0e685000ae8c5a9cdb8811567786
Renato Albuquerque <renatomalb83@gmail.com>
2023-05-03 10:28:39 -0300

    started js codes

---
+++ script.js
@@ -0,0 +1,9 @@
+const rating_1 = document.querySelector("#number-1")
+const rating_2 = document.querySelector("#number-2")
+const rating_3 = document.querySelector("#number-3")
+const rating_4 = document.querySelector("#number-4")
+const rating_5 = document.querySelector("#number-5")
+
+chooseRating() {
+    
+}
